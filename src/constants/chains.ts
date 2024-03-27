506b668fd0979d9c1e1d21e4235d2ccdec76cb98
Myk <89474773+mykcryptodev@users.noreply.github.com>
2024-03-27 19:48:42 -0400

    use coinbase wallet rpc

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -8,6 +8,7 @@ export const SUPPORTED_CHAINS = [
 export const DEFAULT_CHAIN = process.env.NODE_ENV === "production" ? {
   ...base,
   // rpc: "https://api.developer.coinbase.com/rpc/v1/base/A10WpgQTkaHJltNKqtNhT2RQ-E-KgEjs",
+  rpc: "https://chain-proxy.wallet.coinbase.com?targetName=base",
 } : {
   ...baseSepolia,
   // rpc: "https://api.developer.coinbase.com/rpc/v1/base-sepolia/Av-Z_3kkdy_jHZd62vCmczjEfXBPtJPC",
