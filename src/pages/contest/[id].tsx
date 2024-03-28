1d6eb087b3907725967bab0c656eed9a1b693917
Myk <89474773+mykcryptodev@users.noreply.github.com>
2024-03-28 15:41:40 -0400

    media renderer

--- src/pages/contest/[id].tsx
+++ src/pages/contest/[id].tsx
@@ -2,14 +2,14 @@ import { type NextPage, type GetServerSideProps} from "next";
 import { useContext, useState } from "react";
 import ActiveChainContext from "~/contexts/ActiveChain";
 import { api } from "~/utils/api";
-import Image from "next/image";
 import { Leaderboard } from "~/components/Attestation/Leaderboard";
 import { ListAttestations } from "~/components/Attestation/List";
 import AddContestants from "~/components/Contest/AddContestants";
 import { RemoveContestant } from "~/components/Contest/RemoveContestant";
-import { useActiveAccount } from "thirdweb/react";
+import { MediaRenderer, useActiveAccount } from "thirdweb/react";
 import JoinContest from "~/components/Contest/Join";
 import JoinRequestList from "~/components/Contest/JoinRequest/List";
+import { client } from "~/providers/Thirdweb";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { id } = context.params as { id: string };
@@ -75,12 +75,13 @@ export const Contest: NextPage<{id: string}> = ({ id }) => {
           <span className="font-bold">Contestants</span>
           {profiles.map((profile) => (
             <div key={profile.username} className="flex items-center gap-2">
-              <Image
-                src={profile.imgUrl.replace("ipfs://", "https://ipfs.io/ipfs/")}
+              <MediaRenderer
+                src={profile.imgUrl}
                 alt="profile"
-                width={24}
-                height={24}
+                width={"24px"}
+                height={"24px"}
                 className="rounded-full"
+                client={client}
               />
               <div className="flex items-center gap-2">
                 <span className="font-bold text-sm">{profile.username}</span>
