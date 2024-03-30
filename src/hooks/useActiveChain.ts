4d451d9b371a4a8922cfc98f741b25bea91bd16d
Myk <89474773+mykcryptodev@users.noreply.github.com>
2024-03-29 20:13:28 -0400

    no switching

--- src/hooks/useActiveChain.ts
+++ src/hooks/useActiveChain.ts
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import { type Chain } from 'thirdweb';
 
-import { DEFAULT_CHAIN, SUPPORTED_CHAINS } from '~/constants/chains';
+import { DEFAULT_CHAIN } from '~/constants/chains';
 
 const useActiveChain = () => {
-  const [activeChain, setActiveChain] = useState<Chain>(DEFAULT_CHAIN);
+  const [activeChain, setActiveChain] = useState<Chain & { rpcToUpdate: string }>(DEFAULT_CHAIN);
 
   const updateActiveChain = (chainIdOrName: number | string) => {
-    const chainById = SUPPORTED_CHAINS.find(chain => chain.id === chainIdOrName);
-    const chainByName = SUPPORTED_CHAINS.find(chain => chain.name === chainIdOrName);
-    setActiveChain(chainById ?? chainByName ?? DEFAULT_CHAIN);
+    // const chainById = SUPPORTED_CHAINS.find(chain => chain.id === chainIdOrName);
+    // const chainByName = SUPPORTED_CHAINS.find(chain => chain.name === chainIdOrName);
+    // setActiveChain(chainById ?? chainByName ?? DEFAULT_CHAIN);
+    console.log('chainIdOrName', chainIdOrName);
+    setActiveChain(DEFAULT_CHAIN); // no switching chains for now
   }
 
   const updateActiveChainRpc = (rpc: string) => {
