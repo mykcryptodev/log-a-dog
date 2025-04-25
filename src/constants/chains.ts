5d4e1bd6d6c76afa0cbc67469c1ad921d75ce7ff
Mike Solomon <mike.solomon@coinbase.com>
2025-04-25 14:50:27 -0400

    bump tw

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,6 +1,6 @@
 import { base, baseSepolia } from "thirdweb/chains";
  
-export const DEFAULT_CHAIN = process.env.NODE_ENV === "production" ? {
+export const DEFAULT_CHAIN = process.env.NODE_ENV === "production" || process.env.NODE_ENV === "development" ? {
   ...base,
   rpc: "https://api.developer.coinbase.com/rpc/v1/base/A10WpgQTkaHJltNKqtNhT2RQ-E-KgEjs",
   // rpc: "https://chain-proxy.wallet.coinbase.com?targetName=base",
