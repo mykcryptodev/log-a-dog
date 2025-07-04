2467f7adbd561a2ee72d657a2849a3b6c11ffbfb
mykcryptodev <mykcryptodev@gmail.com>
2025-07-04 10:03:06 -0400

    update min stake const

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,7 +7,7 @@ export * from "./chains";
 export const CONTEST_START_TIME = "2025-04-23T12:00:00-04:00"
 export const CONTEST_END_TIME = "2025-09-05T12:00:00-04:00"
 
-export const MINIMUM_STAKE = parseEther("100");
+export const MINIMUM_STAKE = parseEther("300000");
 
 export const APP_NAME = "Log a Dog";
 export const APP_DESCRIPTION = `Earn money eating hotdogs`;
