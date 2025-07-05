9326908e22a1b678f2b394f9f2a73dd65d7e3333
mykcryptodev <mykcryptodev@gmail.com>
2025-07-05 19:44:47 -0400

    advanced

--- src/components/Attestation/Create.tsx
+++ src/components/Attestation/Create.tsx
@@ -334,24 +334,32 @@ export const CreateAttestation: FC<Props> = ({ onAttestationCreated }) => {
                 Advanced
               </div>
               <div className="collapse-content">
-                <textarea
-                  className="textarea textarea-bordered w-full"
-                  placeholder="Add a message to your dog"
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                />
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text text-xs">Description</span>
+                  </label>
+                  <textarea
+                    className="textarea textarea-bordered w-full"
+                    placeholder="Add a message to your dog"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text text-xs">Gas Fees</span>
+                  </label>
+                  <div className="flex items-center gap-2 pl-2">
+                    <input
+                      type="checkbox"
+                      className="checkbox checkbox-xs checkbox-primary"
+                      checked={payOwnGas}
+                      onChange={(e) => setPayOwnGas(e.target.checked)}
+                    />
+                    <span className="text-xs opacity-75">I will pay my own blockchain fees</span>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="form-control">
-              <label className="label cursor-pointer">
-                <span className="label-text text-xs">I will pay my own blockchain fees</span>
-                <input
-                  type="checkbox"
-                  className="checkbox checkbox-xs checkbox-primary"
-                  checked={payOwnGas}
-                  onChange={(e) => setPayOwnGas(e.target.checked)}
-                />
-              </label>
             </div>
           </div>
           <div className="modal-action">
