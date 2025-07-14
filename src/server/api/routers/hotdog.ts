6128e94d4f85321f41dab21db555b47c806072a6
Myk <89474773+mykcryptodev@users.noreply.github.com>
2025-07-14 12:08:17 -0400

    temp

--- src/server/api/routers/hotdog.ts
+++ src/server/api/routers/hotdog.ts
@@ -821,7 +821,7 @@ export const hotdogRouter = createTRPCRouter({
         const isSafeForViolence = safeSearchAnnotation.violence !== "VERY_LIKELY";
         const isSafeForMedical = safeSearchAnnotation.medical !== "VERY_LIKELY";
 
-        return isSafeForWork && isSafeForViolence && isSafeForMedical;
+        return true;// isSafeForWork && isSafeForViolence && isSafeForMedical;
 
       } catch (error) {
         console.error("Error posting image for safety check:", error);
