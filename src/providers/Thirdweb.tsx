6c3ad615c900041375202d819534f2659cbc6ca9
Myk <89474773+mykcryptodev@users.noreply.github.com>
2025-07-14 08:33:19 -0400

    fix thirdweb provider types

--- src/providers/Thirdweb.tsx
+++ src/providers/Thirdweb.tsx
@@ -1,16 +1,15 @@
 import { ThirdwebProvider } from "thirdweb/react";
 import { createThirdwebClient } from "thirdweb";
 import { env } from "~/env";
-import { DEFAULT_CHAIN } from "~/constants";
 
 export const client = createThirdwebClient({
   clientId: env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID,
 });
 
-export const ThirdwebProviderWithDefaultChain = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <ThirdwebProvider client={client} activeChain={DEFAULT_CHAIN}>
-      {children}
-    </ThirdwebProvider>
-  );
+export const ThirdwebProviderWithDefaultChain = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return <ThirdwebProvider>{children}</ThirdwebProvider>;
 };
